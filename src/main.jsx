5c646952c0576dd31f4633f705ec1a9d2f79667f
--replace-all <loretodoris68@gmail.com>
2026-07-24 17:50:01 +0200

    fix: unificar rutas de estilos en minusculas para despliegue

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import './styles/Index-Global.css';
 import './styles/Components.css';
-import './styles/Responsive-ordenador.css';
+import './styles/responsive-ordenador.css';
 import './styles/responsive-movil.css';
 import './styles/Variables.css';
 import './styles/RegistrarComida.css';
